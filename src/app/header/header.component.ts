723616f4e9387d7abce4cc17634a3c3aa5aabbd9
LUIGI071 <tu-correo-de-github@example.com>
2025-08-08 10:57:44 +0200

    dashboard completo, header corregido

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  goToSearch() {
+    // Aquí puedes capturar el valor del input si quieres
+    // Por ejemplo: const query = this.searchInputValue;
+
+    this.router.navigate(['/search']);
+  }
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,9 +9,65 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  @ViewChild('sliderViewport', { static: false }) sliderViewport!: ElementRef;
+  @ViewChild('sliderViewportPlatos', { static: false })
+  sliderViewportPlatos!: ElementRef;
+  @ViewChild('sliderViewportRes', { static: false })
+  sliderViewportRes!: ElementRef;
+  @ViewChild('sliderViewportCoci', { static: false })
+  sliderViewportCoci!: ElementRef;
 
   platos = [
+    {
+      nombre: 'Hamburguesa',
+      img: 'assets/food1.jpg',
+    },
+    {
+      nombre: 'Patatas Bravas',
+      img: 'assets/food2.jpg',
+    },
+    {
+      nombre: 'Tacos',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Arepas',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Empanadas',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Pasta',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Churros',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Ramen',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Pizza',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Asado',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Sushi',
+      img: 'assets/food3.jpg',
+    },
+    {
+      nombre: 'Sopa',
+      img: 'assets/food3.jpg',
+    },
+  ];
+
+  cocinas = [
     { nombre: 'Mediterraneo', img: 'assets/food1.jpg' },
     { nombre: 'Español', img: 'assets/food2.jpg' },
     { nombre: 'Asiatico', img: 'assets/food3.jpg' },
@@ -104,8 +160,12 @@ export class DashboardComponent {
   currentIndex = 0;
   itemsPerPage = 6;
 
+  currentIndexRes = 0;
+  itemsPerPageRes = 6;
+
   scrollSlider(direction: 'left' | 'right') {
-    const container = this.sliderViewport.nativeElement;
+    const container = this.sliderViewportPlatos.nativeElement;
+    if (!container) return;
     const item = container.querySelector('.slider-item');
     if (!item) return;
 
@@ -125,6 +185,56 @@ export class DashboardComponent {
 
     container.scrollTo({
       left: this.currentIndex * scrollAmount,
+      behavior: 'smooth',
+    });
+  }
+
+  scrollSliderCoci(direction: 'left' | 'right') {
+    const container = this.sliderViewportCoci.nativeElement;
+    if (!container) return;
+    const item = container.querySelector('.slider-item');
+    if (!item) return;
+
+    const itemWidth = item.offsetWidth + 40;
+    const scrollAmount = this.itemsPerPage * itemWidth;
+
+    if (direction === 'right') {
+      //Evitar pasar del max scroll
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+      this.currentIndex++;
+      if (this.currentIndex * scrollAmount > maxScrollLeft) {
+        this.currentIndex = Math.floor(maxScrollLeft / scrollAmount);
+      }
+    } else if (direction === 'left' && this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+
+    container.scrollTo({
+      left: this.currentIndex * scrollAmount,
+      behavior: 'smooth',
+    });
+  }
+
+  scrollSliderRes(direction: 'left' | 'right') {
+    const container = this.sliderViewportRes.nativeElement;
+    const item = container.querySelector('.slider-itemRes');
+    if (!item) return;
+
+    const itemWidth = item.offsetWidth + 40;
+    const scrollAmount = this.itemsPerPageRes * itemWidth;
+
+    if (direction === 'right') {
+      const maxScrollLeft = container.scrollWidth - container.clientWidth;
+      this.currentIndexRes++;
+      if (this.currentIndexRes * scrollAmount > maxScrollLeft) {
+        this.currentIndexRes = Math.floor(maxScrollLeft / scrollAmount);
+      }
+    } else if (direction === 'left' && this.currentIndexRes > 0) {
+      this.currentIndexRes--;
+    }
+
+    container.scrollTo({
+      left: this.currentIndexRes * scrollAmount,
       behavior: 'smooth',
     });
   }
